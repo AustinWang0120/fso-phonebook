91b6ae85def2a0f94b01aadee6cf722a5d40d39d
Austin Wang <yyaustin@bu.edu>
2022-10-14 03:52:44 -0400

    solve: part 2b

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Filter = ({ keyword, handleKeywordChange }) => {
+    return (
+        <div>
+            filter shown with
+            <input value={keyword} onChange={handleKeywordChange} />
+        </div>
+    )
 }
 
-export default App;
+const Form = ({
+    handleSubmit,
+    newName,
+    handleNewNameChange,
+    newNumber,
+    handleNewNumberChange,
+}) => {
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                name: <input value={newName} onChange={handleNewNameChange} />
+            </div>
+            <div>
+                number:
+                <input value={newNumber} onChange={handleNewNumberChange} />
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+        </form>
+    )
+}
+
+const List = ({ persons }) => {
+    return (
+        <ul>
+            {persons.map((person) => (
+                <li key={person.name}>
+                    {person.name} {person.number}
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "040-123456", id: 1 },
+        { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+        { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+        { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    ])
+    const [newName, setNewName] = useState("")
+    const [newNumber, setNewNumber] = useState("")
+    const [keyword, setKeyword] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if (persons.map((person) => person.name).includes(newName)) {
+            window.alert(`${newName} is already added to phonebook`)
+        } else {
+            const newPerson = {
+                name: newName,
+                number: newNumber,
+            }
+            setPersons(persons.concat(newPerson))
+            setNewName("")
+            setNewNumber("")
+        }
+    }
+
+    const handleNewNameChange = (event) => setNewName(event.target.value)
+
+    const handleNewNumberChange = (event) => setNewNumber(event.target.value)
+
+    const handleKeywordChange = (event) => setKeyword(event.target.value)
+
+    const personsToShow = persons.filter((person) =>
+        person.name.toLowerCase().includes(keyword.toLowerCase())
+    )
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Filter
+                keyword={keyword}
+                handleKeywordChange={handleKeywordChange}
+            />
+            <h2>Add a new</h2>
+            <Form
+                handleSubmit={handleSubmit}
+                newName={newName}
+                handleNewNameChange={handleNewNameChange}
+                newNumber={newNumber}
+                handleNewNumberChange={handleNewNumberChange}
+            />
+            <h2>Numbers</h2>
+            <List persons={personsToShow} />
+        </div>
+    )
+}
+
+export default App
